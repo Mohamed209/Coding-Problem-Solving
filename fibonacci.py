3a70d621a0b5b8cec1ae3058c40a08168466c455
Mohamed Mossad <mossad@synapse-analytics.io>
2019-04-26 01:07:49 +0200

    solution using dynamic programming

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+# problem :
+# solution using dynmaic programming technique memoization
+def fib(n,memo):
+    if memo[n] is not None:
+        return memo[n]
+    if n==1 or n==2 :
+        return 1
+    else :
+        res=fib(n-1,memo)+fib(n-2,memo)
+        memo[n]=res
+        return res%1000000007 # to nandel very large numbers
+
+N=int(input())
+memo=[None]*(N+1)
+print(fib(N,memo))
