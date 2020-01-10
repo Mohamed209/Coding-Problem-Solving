1d67131807a19fe37369d9400afd4e682d7802ce
mohamed mosad <mohamedmosad209@gmail.com>
2020-01-11 01:23:51 +0200

    updated file with less code

--- next_greater_element.py
+++ next_greater_element.py
@@ -1,48 +1,17 @@
-# Python program to print next greater element using stack
-
-# Stack Functions to be used by printNGE()
-def createStack():
+# code
+T = int(input())
+for t in range(T):
+    N = int(input())
+    array = [int(i)for i in input().split()]
     stack = []
-    return stack
-
-def isEmpty(stack):
-    return len(stack) == 0
-
-def push(stack, x):
-    stack.append(x)
-
-def pop(stack):
-    if isEmpty(stack):
-        print("Error : stack underflow")
-    else:
-        return stack.pop()
-
-'''prints element and NGE pair for all elements of 
-arr[] '''
-def printNGE(arr):
-    s = createStack()
-    element = 0
-    next = 0
-
-    # push the first element to stack
-    push(s, arr[0])
-
-    # iterate for rest of the elements
-    for i in range(1, len(arr)):
-        next=arr[i]
-        if isEmpty(s) == False:
-            element=pop(s)
-            while next>element:
-                print(str(element)+"--> "+str(next))
-                if isEmpty(s) == True:
-                    break
-                element=pop(s)
-            if element>next:
-                push(s,element)
-        push(s, next)
-    while isEmpty(s) == False:
-        element = pop(s)
-        next = -1
-        print(str(element) + "--> " + str(next))
-arr = [11, 13, 21, 3]
-printNGE(arr)
+    NG = {}
+    stack.append(array[0])
+    for j in range(1, N):  # O(n)
+        while (len(stack) != 0) and(array[j] > stack[-1]):
+            NG[stack[-1]] = array[j]
+            stack.pop()
+        stack.append(array[j])
+    for element in stack:
+        NG[element] = -1
+    for el in array:
+        print(NG[el], end=' ')
