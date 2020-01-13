abbc146b9ba3eec4547dc61b390feb3d65a74c8d
mohamed209 <mohamedmosad209@gmail.com>
2020-01-13 20:39:12 +0200

    :sparkles: implemented palindrom checker with recursion and dequeu

---
+++ palindrome_checker.py
@@ -0,0 +1,18 @@
+# problem : https://www.geeksforgeeks.org/c-program-check-given-string-palindrome/
+# solution using dequeu data structure
+from collections import deque
+
+
+def check_palindrom(deque_string, _palindrom=True):
+    if len(deque_string) == 0 or len(deque_string) == 1 and _palindrom:
+        return _palindrom
+    elif deque_string.pop() != deque_string.popleft():
+        _palindrom = False
+        return _palindrom
+    else:
+        return check_palindrom(deque_string)
+
+
+input_string = input()
+de = deque(input_string)
+print(check_palindrom(de))
