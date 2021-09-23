2efd817985de8c4861555a27334e8904aed50e85
mohamed mosad <mohamedmosad209@gmail.com>
2021-09-23 17:06:03 +0200

    Create hackerrank.sql

---
+++ hackerrank.sql
@@ -0,0 +1,3 @@
+problem : https://www.hackerrank.com/challenges/earnings-of-employees/problem
+query : select salary*months as "maximum total earnings" ,count(salary*months) from Employee group by salary*months order by salary*months desc limit 1;
+===============================================================================
