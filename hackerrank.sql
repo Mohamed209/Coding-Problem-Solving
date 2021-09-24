7423604313b687b3e5e80d938e8d3fa8a55533c8
mohamed mosad <mohamedmosad209@gmail.com>
2021-09-24 14:51:43 +0200

    Update hackerrank.sql

--- hackerrank.sql
+++ hackerrank.sql
@@ -1,3 +1,6 @@
 problem : https://www.hackerrank.com/challenges/earnings-of-employees/problem
 query : select salary*months as "maximum total earnings" ,count(1) from Employee group by 1 order by 1 desc limit 1;
 ===============================================================================
+problem : https://www.hackerrank.com/challenges/weather-observation-station-18
+query : select round(abs(min(LAT_N)-max(LAT_N))+abs(min(LONG_W)-max(LONG_W)),4) from STATION
+===============================================================================
