e94a134bbbd3135292fdacd772d956b64b68ca78
mohamed mosad <mohamedmosad209@gmail.com>
2021-09-27 17:55:07 +0200

    Update hackerrank.sql

--- hackerrank.sql
+++ hackerrank.sql
@@ -6,3 +6,7 @@ query : select round(abs(min(LAT_N)-max(LAT_N))+abs(min(LONG_W)-max(LONG_W)),4) 
 ===============================================================================
 PROBLEM : https://www.hackerrank.com/challenges/weather-observation-station-19/problem
 query : select round(sqrt((max(LAT_N)-min(LAT_N))*(max(LAT_N)-min(LAT_N))+(max(LONG_W)-min(LONG_W))*(max(LONG_W)-min(LONG_W))),4) from STATION
+===============================================================================
+problem : https://www.hackerrank.com/challenges/asian-population/problem
+query : select sum(city.population) from city inner join country ON COUNTRY.CODE = CITY.COUNTRYCODE where country.CONTINENT='Asia'
+===============================================================================
