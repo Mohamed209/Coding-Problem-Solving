8a2cb662b9b0fc24fba7e165ba62d889e39ef163
mossad <mohamedmosad209@gmail.com>
2021-11-04 00:48:33 +0200

    :sparkles: daily temp problem

---
+++ Daily Temperatures.py
@@ -0,0 +1,19 @@
+# problem : https://leetcode.com/problems/daily-temperatures/
+from typing import List
+
+
+def dailyTemperatures(temperatures: List[int]) -> List[int]:
+    stack: List[int] = []
+    result: List[int] = [0]*len(temperatures)
+    stack.append(0)
+    for i in range(1, len(temperatures)):
+        while temperatures[i] > temperatures[stack[-1]]:
+            result[stack[-1]] = i-stack[-1]
+            stack.pop()
+            if len(stack) == 0:
+                break
+        stack.append(i)
+    return result
+
+
+dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
