b023a84f8f78fd6c8b3db67c390e20b9368dfafa
mossad <mohamedmosad209@gmail.com>
2021-11-06 01:09:29 +0200

    :sparkles: decode string problem

---
+++ encoded_srting.py
@@ -0,0 +1,25 @@
+# problem : https://leetcode.com/explore/learn/card/queue-stack/239/conclusion/1379/
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        res = ''
+        for i in range(len(s)):
+            if s[i] != ']':
+                stack.append(s[i])
+            else:
+                temps = ''
+                while stack and stack[-1] != '[':
+                    temps = stack.pop()+temps
+                    # if not stack:
+                    #     break
+                stack.pop()
+                multi = ''
+                while stack and stack[-1].isdigit():
+                    multi = stack.pop()+multi
+                    # if not stack:
+                    #     break
+                multi = int(multi)
+                temps = temps*multi
+                stack.append(temps)
+
+        return "".join(stack)
