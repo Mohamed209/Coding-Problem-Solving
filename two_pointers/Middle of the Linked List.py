d96c8142df9a99b327663ba7dc7f9120fbec34d4
mossad <mohamedmosad209@gmail.com>
2022-01-05 23:49:27 +0200

    new problems

---
+++ two_pointers/Middle of the Linked List.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast_ptr = head
+        slow_ptr = head
+        while fast_ptr and fast_ptr.next:
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next
+        return slow_ptr
