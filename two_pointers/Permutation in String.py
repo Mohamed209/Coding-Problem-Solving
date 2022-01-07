faea4dee0cdb599862afda7be7a5d819e607eb01
mossad <mohamedmosad209@gmail.com>
2022-01-07 19:06:58 +0200

    new problems

--- two_pointers/Permutation in String.py
+++ two_pointers/Permutation in String.py
@@ -1,22 +1,15 @@
 # https://leetcode.com/problems/permutation-in-string/
+import collections
+
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        sum_s1 = 0
-        for c in s1:
-            sum_s1 += ord(c)
-        # search s2 for this sum using sliding windows
-        left_ptr = 0
-        right_ptr = 1
-        while right_ptr < len(s2) and left_ptr < len(s2):
-            current_window_sum = sum([ord(c) for c in s2[left_ptr:right_ptr+1]])
-            if current_window_sum == sum_s1:
+        s1_counter = collections.Counter(s1)
+
+        s1_len = len(s1)
+
+        for i in range(len(s2) - s1_len + 1):
+            if collections.Counter(s2[i:i+s1_len]) == s1_counter:
                 return True
-            elif current_window_sum < sum_s1:
-                right_ptr += 1
-            else:
-                left_ptr += 1
+
         return False
-
-
-s = Solution()
-print(s.checkInclusion("ab", "ba"))
