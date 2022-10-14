40090ff18cc54c03d3da255e044e056b90245a75
mossad <mohamedmosad209@gmail.com>
2022-10-14 15:07:40 +0200

    https://leetcode.com/problems/decode-ways/

---
+++ dynamic_programming/Decode Ways.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/decode-ways/
+class Solution:
+    @staticmethod
+    def dfs(s: str, index: int, cache: dict):
+        if cache[index]:
+            return cache[index]
+        if index == len(s):
+            return 1
+        elif s[index] == "0":
+            return 0
+        result = Solution.dfs(s, index+1, cache)
+        if index + 1 < len(s) and int(s[index:index+2]) <= 26:
+            result += Solution.dfs(s, index+2, cache)
+        cache[index] = result
+        return result
+
+    def numDecodings(self, s: str) -> int:
+        cache = {i: None for i in range(len(s)+1)}
+        return Solution.dfs(s, 0, cache)
