1c985ed854ad334e3cadb20e84531c0960b03e64
mossad <mohamedmosad209@gmail.com>
2022-10-24 22:41:43 +0200

    tabulation canSum

--- dynamic_programming/Can Sum.py
+++ dynamic_programming/Can Sum.py
@@ -25,8 +25,34 @@ def canSum(target: int, nums: List[int]):
     return False
 
 
-#print(canSum(7, [2, 3]))
-#print(canSum(7, [3, 5, 4, 7]))
-#print(canSum(7, [2, 4]))
-#print(canSum(8, [2, 3, 5]))
+print(canSum(7, [2, 3]))
+print(canSum(7, [3, 5, 4, 7]))
+print(canSum(7, [2, 4]))
+print(canSum(8, [2, 3, 5]))
+print(canSum(300, [7, 14]))
+
+print("#"*10)
+# Tabulation
+
+
+def canSum(target: int, nums: List[int]):
+    # populate table with false
+    table = [False]*(target+1)
+    table[0] = True
+    """
+    1- every element in nums should have true in table as it can be constructed from the array
+    2- at any index i in the table , i+n for n in nums should be true
+    """
+    for i in range(0, len(table)):
+        if table[i] == True:
+            for n in nums:
+                if i+n < len(table):
+                    table[i+n] = True
+    return table[-1]
+
+
+print(canSum(7, [2, 3]))
+print(canSum(7, [3, 5, 4, 7]))
+print(canSum(7, [2, 4]))
+print(canSum(8, [2, 3, 5]))
 print(canSum(300, [7, 14]))
