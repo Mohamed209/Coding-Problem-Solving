66049a45b046e96bb7fa4d0e227f8aef0cae409b
Mohamed209 <mohamedmosad209@gmail.com>
2023-02-19 13:15:18 +0200

    🐛 cache in init method

--- dynamic_programming/Word Break.py
+++ dynamic_programming/Word Break.py
@@ -5,22 +5,23 @@ from typing import List
 
 
 class Solution:
-    cache = {}
+    def __init__(self) -> None:
+        self.cache = {}
 
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         if len(s) == 0:
             return True
-        if s in Solution.cache:
-            return Solution.cache[s]
+        if s in self.cache:
+            return self.cache[s]
         for word in wordDict:
             new_subword = s.removeprefix(word)
             # if removeprefix method returned the same string , this means that (word) is not prefix of (s)
             if len(new_subword) == len(s):
                 continue  # move and try next word from wordDict , as current word is not prefix of s
             if self.wordBreak(new_subword, wordDict):
-                Solution.cache[new_subword] = True
+                self.cache[new_subword] = True
                 return True
-        Solution.cache[new_subword] = False
+        self.cache[new_subword] = False
         return False
 
 
