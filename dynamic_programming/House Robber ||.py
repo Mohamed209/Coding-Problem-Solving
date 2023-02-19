202cf5a9aed7dab25224ce00ba4c0592121da62b
Mohamed209 <mohamedmosad209@gmail.com>
2023-02-19 14:48:24 +0200

    ✨ https://leetcode.com/problems/house-robber-ii/

---
+++ dynamic_programming/House Robber ||.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/house-robber-ii/
+from typing import List
+
+
+class Solution:
+    cache = {}
+
+    @staticmethod
+    def dfs(nums: tuple[int], max_loot=0):
+        if nums in Solution.cache:
+            return Solution.cache[nums]
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums)
+        """
+        now ,for me as professional theif :D , which is better : 
+        1- steal current house (nums[0]) + recurse again on houses starting from my starting point+2
+        2- steal next house (nums[1]) + recurse again on houses starting from my starting point+3
+        """
+        max_loot += max(nums[0]+Solution.dfs(nums[2:], max_loot),
+                        nums[1]+Solution.dfs(nums[3:]), max_loot)
+        Solution.cache[nums] = max_loot
+        return max_loot
+
+    def rob(self, nums: List[int]) -> int:
+        # convert nums to tuple , to be used as key in caching dict (apply memoaization)
+        if len(nums) == 1:
+            return nums[0]
+        left_to_right = Solution.dfs(tuple(nums[:-1]))
+        right_to_left = Solution.dfs(tuple(nums[-1:-len(nums):-1]))
+        return max(left_to_right, right_to_left)
+
+
+s = Solution()
+print(s.rob([1, 2]))
+print(s.rob([2, 3, 2]))
+print(s.rob([1, 2, 3, 1]))
+print(s.rob([1, 2, 3]))
