cc98bda37d33072f0b7b03ba6ba1e2afee75fc3c
Mohamed209 <mohamedmosad209@gmail.com>
2023-02-19 18:51:01 +0200

    ✨ https://leetcode.com/problems/palindromic-substrings

--- dynamic_programming/Palindromic Substrings.py
+++ dynamic_programming/Palindromic Substrings.py
@@ -1,18 +1,28 @@
-# https://leetcode.com/problems/palindromic-substrings/
-# revisit
+# https://leetcode.com/problems/palindromic-substrings
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = 0
-
+        """
+        If “aba” is a palindrome, is “xabax” a palindrome? Similarly is “xabay” a palindrome?
+        """
+        cnt = 0
         for i in range(len(s)):
-            res += self.countPali(s, i, i)
-            res += self.countPali(s, i, i + 1)
-        return res
+            # assume current char is the center char
+            l, r = i, i
+            # expand window left and right with one char
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                cnt += 1
+                l -= 1
+                r += 1
+            # assume current two chars in the middle is the center char
+            l, r = i, i+1
+            # expand window left and right with one char
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                cnt += 1
+                l -= 1
+                r += 1
+        return cnt
 
-    def countPali(self, s, l, r):
-        res = 0
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            res += 1
-            l -= 1
-            r += 1
-        return res
+
+s = Solution()
+print(s.countSubstrings("abc"))
+print(s.countSubstrings("aaa"))
