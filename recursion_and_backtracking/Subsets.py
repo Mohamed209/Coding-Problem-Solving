eb2d24e69b5de0b8150c750fd7b8664cdde81cac
Mohamed209 <mohamedmosad209@gmail.com>
2023-03-01 21:29:24 +0200

    ✨ https://leetcode.com/problems/subsets/

--- recursion_and_backtracking/Subsets.py
+++ recursion_and_backtracking/Subsets.py
@@ -3,13 +3,27 @@ from typing import List
 
 
 class Solution:
+    def __init__(self) -> None:
+        self.result = []
+        self.current_path = []
+        self.cache = {}
+
+    def dfs(self, nums: List[int]) -> None:
+        if len(nums) == 0:
+            if tuple(self.current_path.copy()) in self.cache:
+                return
+            self.result.append(self.current_path.copy())
+            self.cache[tuple(self.current_path.copy())] = True
+            return
+        for i in range(len(nums)):
+            self.current_path.append(nums[i])
+            self.dfs(nums[i+1:])  # take current element
+            self.current_path.pop()  # not to take current element
+            self.dfs(nums[i+1:])
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for num in nums:
-            for ex in res[1:]:
-                res.append(ex+[num])
-            res.append([num])
-        return res
+        self.dfs(nums)
+        return self.result
 
 
 s = Solution()
