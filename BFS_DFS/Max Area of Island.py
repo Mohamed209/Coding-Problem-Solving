a44493746919b65b51cb3abfaa2dc3be8e209e19
Mohamed209 <mohamedmosad209@gmail.com>
2023-03-20 20:35:37 +0200

    my https://leetcode.com/problems/max-area-of-island/

--- BFS_DFS/Max Area of Island.py
+++ BFS_DFS/Max Area of Island.py
@@ -3,24 +3,51 @@ from typing import List
 
 
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        max_area = 0
-        visited = set()
+    def __init__(self) -> None:
+        self.visited = set()
+        self.cur_island_area = 0
 
-        def dfs(i, j):
-            if i == len(grid) or i < 0 or j == len(grid[0]) or j < 0 or (i, j) in visited or grid[i][j] == 0:
-                return 0
-            visited.add((i, j))
-            return 1+(dfs(i-1, j)+dfs(i+1, j)+dfs(i, j-1)+dfs(i, j+1))
-        # first we have to visit every cell in the grid
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    # apply dfs on this cell
-                    max_area = max(dfs(i, j), max_area)
-        return max_area
+    def dfs(self, r, c, grid):
+        # base cases to stop rcursive dfs
+        if (
+            r < 0
+            or r >= len(grid)
+            or c < 0
+            or c >= len(grid[0])
+            or grid[r][c] == 0
+            or (r, c) in self.visited
+        ):
+            return 0
+        self.visited.add((r, c))
+        # start 4 directions dfs summing the paths
+        return (
+            1
+            + self.dfs(r, c - 1, grid)
+            + self.dfs(r, c + 1, grid)
+            + self.dfs(r - 1, c, grid)
+            + self.dfs(r + 1, c, grid)
+        )
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        res = []
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                res.append(self.dfs(r, c, grid))
+        return max(res)
 
 
 s = Solution()
-print(s.maxAreaOfIsland([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [
-    0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]))
+print(
+    s.maxAreaOfIsland(
+        grid=[
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+        ]
+    )
+)
