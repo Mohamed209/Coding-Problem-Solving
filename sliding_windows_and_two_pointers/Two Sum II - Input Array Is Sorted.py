5573b6552506c53514a94fe0faef36ea7793adc6
Mohamed209 <mohamedmosad209@gmail.com>
2023-06-29 18:59:53 +0300

    better comments

--- sliding_windows_and_two_pointers/Two Sum II - Input Array Is Sorted.py
+++ sliding_windows_and_two_pointers/Two Sum II - Input Array Is Sorted.py
@@ -5,13 +5,16 @@ from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left_ptr = 0
-        right_ptr = len(numbers)-1
-        while(left_ptr != right_ptr):
-            if numbers[left_ptr]+numbers[right_ptr] > target:
-                # move right_ptr to the left
+        right_ptr = len(numbers) - 1
+        while left_ptr != right_ptr:
+            if numbers[left_ptr] + numbers[right_ptr] > target:
+                # possibly the pair sum should be in the left subarray of nums in the poor area :) with small numbers
+                # move right ptr to the left
                 right_ptr -= 1
-            elif numbers[left_ptr]+numbers[right_ptr] < target:
-                # ove left_ptr to the right:
+            elif numbers[left_ptr] + numbers[right_ptr] < target:
+                # possibly pair sum should be in the right subarray , in the rich area :) with big numbers
+                # move left ptr to the right
                 left_ptr += 1
             else:
-                return [left_ptr+1, right_ptr+1]
+                return [left_ptr + 1, right_ptr + 1]
+        return [-1, -1]
