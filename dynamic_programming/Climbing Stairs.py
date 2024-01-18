ed4e963d5f200b6b8f50ba810c04888dedce0516
Mohamed209 <mohamedmosad209@gmail.com>
2024-01-18 15:19:32 +0200

    https://leetcode.com/problems/climbing-stairs/

--- dynamic_programming/Climbing Stairs.py
+++ dynamic_programming/Climbing Stairs.py
@@ -1,13 +1,20 @@
 # https://leetcode.com/problems/climbing-stairs/
+from functools import cache
+
 
 class Solution:
-    cache = {}
-
     def climbStairs(self, n: int) -> int:
-        if n in Solution.cache:
-            return Solution.cache[n]
-        if n in [0, 1]:
-            return 1
-        result = self.climbStairs(n-1)+self.climbStairs(n-2)
-        Solution.cache[n] = result
-        return result
+        @cache
+        def dfs(n):
+            # base case
+            if n == 0:
+                return 1  # there is a valid path to jump the n stairs
+            if n < 0:
+                return 0  # current path is not valid one
+            return dfs(n - 1) + dfs(n - 2)
+
+        return dfs(n)
+
+
+s = Solution()
+s.climbStairs(n=3)
