5f8bbbac1a9375b83c41913ad392173b03cfb872
mohamed209 <mohamedmosad209@gmail.com>
2025-02-01 13:41:01 +0200

    review

--- binary_trees/Binary Tree Right Side View.py
+++ binary_trees/Binary Tree Right Side View.py
@@ -1,3 +1,15 @@
+import collections
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         # base case
